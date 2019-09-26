5c3a50a5476b0b904f2337bda188a430f6f44212
Luka Sarich <lukasarichhh@gmail.com>
2019-09-26 16:23:13 +0200

    Search and print method added

--- js/quiz.js
+++ js/quiz.js
@@ -1,45 +1,57 @@
-var questions = [
-  ['How many states are in the United States?', 50],
-  ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6],
+var students = [
+
+  {name : 'Luka', track: 'IOS', achievements: 420, points: 69000},
+  {name : 'John', track: 'Python', achievements: 5420, points: 70000},
+  {name : 'Ashley', track: 'Script', achievements: 9420, points: 1000},
+  {name : 'Rino', track: 'Java', achievements: 2420, points: 42000},
+  {name : 'Haikey', track: 'Web Design', achievements: 1420, points: 42900}
+
 ];
 
-var correctAnswers = 0;
-var questions;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var correctAnswers = 0;
+var question;
 var answer;
 var response;
-var correct = [];
-var wrong = [];
-
 
 function print(message) {
   document.write(message);
 }
 
-function buildList(arr) {
-  var listHTML = '<ol>';
-    for (var i = 0; i < arr.length; i += 1) {
-      listHTML += '<li>' + arr[i] + '</li>';
-    }
-    listHTML += '</ol>';
-    return listHTML;
-}
-
 for (var i = 0; i < questions.length; i += 1) {
-  question = questions[i][0];
-  answer = questions[i][1];
-  response =parseInt(prompt(question));
+  question = questions[i].question;
+  answer = questions[i].answer;
+  response = prompt(question);
+  response = parseInt(response);
   if (response === answer) {
-    correctAnswers += 1;\
-    correct.push(question)
-  } else {
-    wrong.push(question);
+    correctAnswers += 1;
   }
 }
 
-html = "You got " + correctAnswers + " question(s) right.";
-html += '<h2> You got these questions correct :</h2>';
-html += buildList(correct);
-html += '<h2> You got these questions wrong:</h2>';
-html += buildList(wrong);
-print(html)
+html = "You got " + correctAnswers + " question(s) right."
+print(html);
+*/
